e60a69adc758a60134dcc10c680cf3fc1721f7a8
Simon LOIR <simon.loir@student.unamur.be>
2022-07-10 14:37:53 +0200

    boileplate

---
+++ src/components/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { useRouter } from '../router';
+
+export default function App({ children }: { children?: React.ReactNode }) {
+    return <>{children}</>;
+}
---
+++ src/router.tsx
@@ -0,0 +1,41 @@
+import { Component, createContext, ReactNode, useContext } from 'react';
+import App from './components/App';
+import HomePage from './components/HomePage';
+
+interface RouterState {
+    route: string;
+}
+
+const RouterContext = createContext<Router | undefined>(undefined);
+
+export function useRouter() {
+    const router: Router = useContext(RouterContext) as Router;
+    return router;
+}
+
+export default class Router extends Component {
+    get name() {
+        return 'hello';
+    }
+
+    state: Readonly<RouterState> = {
+        route: '/',
+    };
+
+    render(): ReactNode {
+        let screen: ReactNode = <>404 - The requested route does not exist</>;
+        switch (this.state.route) {
+            case '/':
+                screen = <HomePage />;
+                break;
+
+            default:
+                break;
+        }
+        return (
+            <RouterContext.Provider value={this}>
+                <App>{screen}</App>
+            </RouterContext.Provider>
+        );
+    }
+}
